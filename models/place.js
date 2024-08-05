02682eb4d92c0c4fc26e637d2e2ac9ca5f18edf7
M Rifqi <muhamadrifqi.id@gmail.com>
2024-08-05 21:56:18 +0700

    membuat model review dan membuat relasi

--- models/place.js
+++ models/place.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const Review = require('./review')
 
 const placeSchema = new Schema({
     title: String,
     price: Number,
     description: String,
     location: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
 
 module.exports = mongoose.model('Place', placeSchema)
