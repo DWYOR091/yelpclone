24b2bd63ba162f14bbb4b58723252ff3cb55e599
M Rifqi <muhamadrifqi.id@gmail.com>
2024-08-08 15:11:12 +0700

    update place validasi

--- routes/place.js
+++ routes/place.js
@@ -6,10 +6,10 @@ const ErrorHandler = require('../utils/ErrorHandler')
 
 //validasi
 const validasiPlace = (req, res, next) => {
-    const { error } = placeSchema.validate()
+    const { error } = placeSchema.validate(req.body)
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
-        return new ErrorHandler(msg, 400)
+        return next(new ErrorHandler(msg, 400))
     } else {
         next()
     }
