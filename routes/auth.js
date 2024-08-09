751c17fe1e8d58ee7edb3b3a5ce3735a0afbb014
M Rifqi <muhamadrifqi.id@gmail.com>
2024-08-09 13:41:06 +0700

    membuat middleware jika auth redirect login dan register tidak bisa

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
+const ensureNotAuth = require('../middlewares/redirectIfAuth')
 
-router.get('/register', (req, res) => {
+router.get('/register', ensureNotAuth, (req, res) => {
     res.render('auth/register')
 })
 
@@ -11,16 +12,20 @@ router.post('/register', async (req, res, next) => {
     try {
         const { username, email, password } = req.body
         const user = new User({ username, email })
-        await User.register(user, password)
-        req.flash('success-msg', 'Registered Successfully, Now You Can Log In!')
-        res.redirect('/login')
+        const registerUser = await User.register(user, password)
+        req.login(registerUser, (err) => {
+            err ? next(err) : ''
+            req.flash('success-msg', 'Registered Successfully, Now You Log In!')
+            res.redirect('/places')
+        })
+
     } catch (error) {
         req.flash('error-msg', error.message)
         res.redirect('/register')
     }
 })
 
-router.get('/login', (req, res) => {
+router.get('/login', ensureNotAuth, (req, res) => {
     res.render('auth/login')
 })
 
